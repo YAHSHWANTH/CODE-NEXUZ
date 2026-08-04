30377004e512dee7d140055d4d0ebaf33b63a303
Yaswanth <yaswanth1212000@gmail.com>
2026-08-04 14:37:28 +0530

    Update Navbar logo filename to logoo.png as uploaded by the user to the private repository

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
         
         {/* Logo + Gradient Text */}
         <div className="flex items-center space-x-3">
-          <img src="/logoo.jpg" alt="KodNexuz Logo" className="h-10 w-auto" />
+          <img src="/logoo.png" alt="KodNexuz Logo" className="h-10 w-auto" />
           <span
             className="text-xl font-bold bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent"
             style={{ fontFamily: "Garalama, sans-serif" }}
