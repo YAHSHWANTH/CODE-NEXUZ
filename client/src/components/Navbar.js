18eda179dac665eba7d10c06996244dc6db9c182
Yaswanth <yaswanth1212000@gmail.com>
2026-08-04 14:58:54 +0530

    Comment out brand name text in Navbar instead of deleting it

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -34,6 +34,12 @@ const Navbar = () => {
         {/* Logo */}
         <div className="flex items-center space-x-3">
           <img src="/logoo.png" alt="KodNexuz Logo" className="h-10 w-auto" />
+          {/* <span
+            className="text-xl font-bold bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent"
+            style={{ fontFamily: "Garalama, sans-serif" }}
+          >
+            KodNexuz
+          </span> */}
         </div>
 
         {/* Desktop Navigation Links */}
